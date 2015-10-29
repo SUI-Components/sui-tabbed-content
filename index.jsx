817af140d7d1f119359d08216587f7537965cd8d
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-10-29 16:38:08 +0100

    Updates

---
+++ index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import TabbedContent from '../src';
+import './style.scss';
+import '../src/index.scss';
+
+class SampleComponent extends React.Component {
+  render() {
+    return (<p>{this.props.content}</p>);
+  }
+}
+
+SampleComponent.propTypes = {
+  content: React.PropTypes.string
+};
+
+const blocks = [{
+  title: 'First component',
+  selected: true,
+  component: <SampleComponent content={'First Component'} />
+}, {
+  title: 'Second component',
+  component: <SampleComponent content={'Second Component'} />
+}, {
+  title: 'Third component',
+  component: <SampleComponent content={'Third Component'} />
+}];
+
+ReactDom.render(<TabbedContent blocks={blocks}/>, document.getElementById('main'));
